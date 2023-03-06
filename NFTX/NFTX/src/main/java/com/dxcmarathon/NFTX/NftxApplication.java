4170264f1caf8945abbfac00e4c97a7d04cac47a
ivorum <ivailo.rumenov@outlook.com>
2023-03-06 16:56:24 +0200

    Fist commit to set up the Springweb server.

---
+++ NFTX/NFTX/src/main/java/com/dxcmarathon/NFTX/NftxApplication.java
@@ -0,0 +1,13 @@
+package com.dxcmarathon.NFTX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftxApplication.class, args);
+	}
+
+}
---
+++ NFTX/NFTX/src/test/java/com/dxcmarathon/NFTX/NftxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxcmarathon.NFTX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
